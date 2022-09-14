dd44e143f721f9a0dbd08294549fdddd8a72e4e4
Maria Scheglova <91660359+Marichka2003@users.noreply.github.com>
2022-09-14 22:43:09 +0300

    Added Homework_4

---
+++ Marysa2003_Homework_4/Marysa2003_Homework4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marysa2003_Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] person = new Person[6];
+            for (int i = 0; i < person.Length; i++)
+            {
+                person[i] = new Person();
+                person[i].Input(i);
+            }
+            for (int i = 0; i < person.Length; i++)
+            {
+                
+                person[i].Age();
+                person[i].Output();
+                if (person[i].age < 16)
+                {
+                    person[i].ChangeName(person,i);
+                    Console.WriteLine(person[i]);
+                }
+            }
+            for (int i = 0; i < person.Length; i++)
+            {
+                person[i].Output();
+            }
+
+
+            for (int i = 0; i < person.Length; i++)
+            {
+                for (int j = i + 1; j < person.Length; j++)
+                {
+                    if (person[i] == person[j])
+                    {
+                        Console.WriteLine($"The same name is : {person[i]}");
+                    }
+                    else
+                    { Console.WriteLine($"There aren't the same names"); }
+                }
+            }
+
+
+        }
+    }
+}
