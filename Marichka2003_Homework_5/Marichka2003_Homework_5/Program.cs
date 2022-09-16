2cb34cd39363e9c38c0f2319fb99dae41458e9dd
Maria Scheglova <91660359+Marichka2003@users.noreply.github.com>
2022-09-16 19:57:11 +0300

    Added HW5

---
+++ Marichka2003_Homework_5/Marichka2003_Homework_5/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Marichka2003_Homework_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IDeveloper> developers = new List<IDeveloper>();
+           
+            developers.Add(new Programmer("Maria", "C#"));
+            developers.Add(new Programmer("Ann", "Java"));
+            developers.Add(new Builder("Sasha", " roof"));
+            developers.Add(new Builder("Alex", " ceiling"));
+
+            Console.WriteLine("Programmers");
+            foreach (IDeveloper i in developers)
+            {
+                if (i.GetType() == typeof(Programmer))
+                {
+                    i.Create();
+                    
+                }
+               
+            }
+            Console.WriteLine();
+            Console.WriteLine("Builders");
+            foreach (IDeveloper i in developers)
+            {
+                if (i.GetType() == typeof(Builder))
+                {
+                    i.Create();
+                    
+                }
+
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Sort by name :");
+            developers.Sort();
+            foreach (IDeveloper i in developers)
+            {
+                Console.WriteLine(i.Tool);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Your list are cleaned");
+            foreach (IDeveloper i in developers)
+            {
+                i.Destroy();
+            }
+        }
+    }
+}
---
+++ Marichka2003_Homework_5_2/Marichka2003_Homework_5_2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Marichka2003_Homework_5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> persons = new Dictionary<int, string>();
+            bool ifTrue = true;
+            int i = 0;
+            while(ifTrue)
+            {
+                try
+                {
+                    for (; i < 7; i++)
+                    {
+                        Console.Write("Enter the ID of person : ");
+                        int number = Convert.ToInt32(Console.ReadLine());
+                        foreach (var item in persons)
+                        {
+                            if (number == item.Key)
+                            {
+                                Console.WriteLine("You enter duplicate ID!");
+                                Console.Write("Enter the ID of person : ");
+                                number = Convert.ToInt32(Console.ReadLine());
+                            }
+                        }
+                        Console.Write("Enter the name of person : ");
+                        string name = Console.ReadLine();
+
+                        persons.Add(number, name);
+
+                        if(i == 6)
+                        {
+                            ifTrue = false;
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    Debug.WriteLine("Exception Message");
+                }
+            }
+            Console.Clear();
+            ifTrue = true;
+            foreach (var item in persons)
+            {
+                Console.WriteLine($"{item.Key} {item.Value} ");
+            }
+
+
+            Console.Write("Enter the ID: ");
+            int ID = Convert.ToInt32(Console.ReadLine());
+            foreach (var item in persons)
+            {
+                if (ID == item.Key)
+                {
+                    Console.WriteLine($"This ID has name : {item.Value}");
+                    ifTrue = false;
+                }
+            }
+            if (ifTrue)
+            {
+                Console.WriteLine("There aren't the such ID");
+            }
+
+        }
+    }
+}
