bbfc4acc00247ae93d3dd6847627828b22a69dea
yuzkivtaras <98391159+yuzkivtaras@users.noreply.github.com>
2022-09-18 20:46:22 +0300

    Add files via upload

---
+++ Yuzkiv_HW5/Builder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_5
+{
+    public class Builder : IDeveloper, IComparable<IDeveloper>
+    {
+        public string Tool { get; set; }
+
+        public Builder(string tool)
+        {
+            Tool = tool;
+        }
+
+        public void Create()
+        {
+            Console.WriteLine($"Builder: {Tool} was created");
+        }
+
+        public void Destroy()
+        {
+            Console.WriteLine($"Builder: {Tool} was destroyed");
+        }
+
+        int IComparable<IDeveloper>.CompareTo(IDeveloper other)
+        {
+            if (this.Tool.Length > other.Tool.Length)
+            {
+                return 1;
+            }             
+            else if (this.Tool.Length < other.Tool.Length)
+            {
+                return -1;
+            }              
+            if (this.Tool.Length == other.Tool.Length)
+            {
+                return 0;
+            }
+               
+            return 0;
+        }
+    }
+}
---
+++ Yuzkiv_HW5/IDeveloper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_5
+{
+    public interface IDeveloper : IComparable<IDeveloper>
+    {
+        string Tool { get; set; }
+
+        void Create();
+
+        void Destroy();
+    }
+}
