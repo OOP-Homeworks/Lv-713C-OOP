fec6b0b0999cf25ff9e3c0aebde5191e6d7c4329
OlegShokun <shokunoleg@gmail.com>
2022-09-18 15:48:18 +0300

    Add HW5

---
+++ ShokunHomework/HW5/ShokunHW5/Builder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShokunHW5
+{
+    internal class Builder : IDeveloper, IComparable<IDeveloper>
+    {
+        string tool;
+        string name;
+
+        public string Tool
+        {
+            get { return tool; }
+            set { tool = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Builder(string name, string tool)
+        {
+            this.name = name;
+            this.tool = tool;
+        }
+
+        public void Create()
+        {
+            Console.WriteLine($"{name} working with {tool}");
+        }
+
+        public void Destroy()
+        {
+            Console.WriteLine($"{name} can destroy the toilet with {tool}");
+        }
+
+        int IComparable<IDeveloper>.CompareTo(IDeveloper other)
+        {
+            return String.Compare(this.Tool, other.Tool);
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"Name {name}, builder's tool: {tool}");
+        }
+    }
+}
---
+++ ShokunHomework/HW5/ShokunHW5/Programmer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShokunHW5
+{
+    internal class Programmer : IDeveloper, IComparable<IDeveloper>
+    {
+        string language;
+        string name;
+
+        public string Tool
+        { 
+            get { return language; }
+            set { language = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Programmer(string name, string language)
+        {
+            this.name = name;
+            this.language = language;
+        }
+
+
+        public void Create()
+        {
+            Console.WriteLine($"{name} codes in {language}");
+
+        }
+
+        public void Destroy()
+        {
+            Console.WriteLine($"{name} can destroy your business with {language}");
+        }
+
+        int IComparable<IDeveloper>.CompareTo(IDeveloper other)
+        {
+            return String.Compare(this.Tool, other.Tool);
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"Name {name}, programming language: {language}");
+        }
+    }
+}
