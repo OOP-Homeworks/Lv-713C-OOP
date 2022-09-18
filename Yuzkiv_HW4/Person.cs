7e59cad38547b18462006388c1c7cb149e8f5624
yuzkivtaras <98391159+yuzkivtaras@users.noreply.github.com>
2022-09-18 20:44:55 +0300

    Add files via upload

---
+++ Yuzkiv_HW4/Person.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_4
+{
+    public class Person
+    {
+        private string name { get; set; }
+        private int birthYear { get; set; }
+
+        public Person()
+        {
+            name = "Mark";
+            birthYear = 1995;
+        }
+
+        public Person(string name, int birthYear)
+        {
+            this.name = name;
+            this.birthYear = birthYear;
+        }
+
+        public int Age()
+        {
+            int age = DateTime.Now.Year - birthYear;
+            return age;
+        }
+
+        public static Person Input()
+        {
+            Console.Write("Please, enter name: ");
+            string name = Console.ReadLine();
+            Console.Write("Please, enter year of birth: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Person person = new Person(name, age);
+            return person;
+        }
+
+        public void ChangeName()
+        {
+            if (Age() < 16)
+            {
+                name = "Very Young";
+            }          
+        }
+
+        public void Output()
+        {
+            Console.WriteLine($"Person's name: {name}, Person's age: {Age()}");
+        }
+
+        public static bool operator == (Person first, Person second)
+        {
+            return (first.name == second.name);
+        }
+
+        public static bool operator != (Person first, Person second)
+        {
+            return !(first == second);
+        }
+
+        public override string ToString()
+        {
+            return "\nPerson's name: " + name + " " + "Person's age: " + Age();
+        }
+    }
+}
---
+++ Yuzkiv_HW4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Output();
+
+            Person person1 = new Person("John", 2000);
+            person1.Output();
+
+            var persons = new Person[6];
+            persons[0] = person;
+            persons[1] = person1;
+
+            for (int i = 2; i < persons.Length; i++)
+            {
+                persons[i] = Person.Input();
+            }
+
+            for (int i = 0; i < persons.Length; i++)
+            {
+                persons[i].Output();
+                persons[i].ChangeName();
+            }
+
+            for (int i = 0; i < persons.Length; i++)
+            {
+                Console.WriteLine(persons[i].ToString());
+            }
+
+            for (int i = 0; i < persons.Length; i++)
+            {
+                for (int j = i + 1; j < persons.Length; j++)
+                {
+                    if (persons[i] == persons[j])
+                    {
+                        Console.WriteLine($"{persons[i]} and {persons[j]} are same names");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{persons[i]} and {persons[j]} aren't same names");
+                    }
+                }
+            }
+        }
+    }
+}
