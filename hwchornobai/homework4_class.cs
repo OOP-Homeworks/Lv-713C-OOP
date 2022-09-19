039890d3952d2e76f169e19359eb66758980475c
Marian Chornobai <112644522+itblackbai@users.noreply.github.com>
2022-09-19 14:25:54 +0300

    homework4_chornobai

---
+++ hwchornobai/homework4_class.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace learnqw
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+            class Person
+        {
+            private string name;
+            public string Name
+            {
+                set { name = value; }
+                get { return name; }
+            }
+            private int birthYear;
+            public int PersonAge
+            {
+                set { birthYear = value; }
+                get { return birthYear; }
+            }
+
+            public int Person_Age;
+
+            public Person() { }
+            public Person(string n, int b) { name = n; b = birthYear; }
+
+            public void Person_age()
+            {
+                Person_Age = 2022 - birthYear;
+                Console.WriteLine($"Person_age :   Name is {name} && age {Person_Age}");
+
+            }
+
+            public void Input(int i)
+            {
+                Console.WriteLine("Enter the name ");
+                name = Console.ReadLine();
+                Console.WriteLine("Enter the birhyear ");
+                birthYear = int.Parse(Console.ReadLine());
+            }
+            public void ChangeName(Person[] person, int i)
+            {
+                Console.WriteLine($"Change {person[i]} to ");
+                name = "Very Young";
+            }
+            public override string ToString()
+            {
+                return "Name is :" + name + "  BirhYear is :" + birthYear;
+            }
+            public void Output()
+            {
+
+                Console.WriteLine($"Person name " + name + "Birth Year " + birthYear);
+            }
+            public static bool operator ==(Person first, Person second)
+            {
+                return (first.name == second.name);
+            }
+            public static bool operator !=(Person first, Person second)
+            {
+                return !(first.name == second.name);
+            }
+        }
+    }
+}
+
---
+++ hwchornobai/homework4_main.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace learnqw
+{
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] person = new Person[6];
+            for (int i = 0; i < person.Length; i++)
+            {
+                person[i] = new Person();
+                person[i].Input(i);
+            }
+            for (int i = 0; i < person.Length; i++)
+            {
+                person[i].Person_age();
+                person[i].Output();
+                if (person[i].Person_Age < 16)
+                {
+                    person[i].ChangeName(person, i);
+                    Console.WriteLine($"Change name {person[i]}");
+                }
+            }
+            for (int i = 0; i < person.Length; i++)
+            {
+                person[i].Output();
+            }
+            for (int i = 0; i < person.Length; i++)
+            {
+                for (int q = i + 1; q < person.Length; q++)
+                {
+                    if (person[i] == person[q])
+                    {
+                        Console.WriteLine($"Same name is :  {person[i]}");
+                    }
+                    else
+                    { Console.WriteLine($"There aren't same name!"); }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
