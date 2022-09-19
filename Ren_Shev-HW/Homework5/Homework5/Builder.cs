1ec73f1b57370118a079d6cfda0cf3f8662ba8ed
Shevchuk-Ren <shevchuk_re@ukr.net>
2022-09-19 20:02:04 +0100

    hw5 is complited

---
+++ Ren_Shev-HW/Homework5/Homework5/Builder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Homework5
+{
+    internal class Builder: IDeveloper, IComparable<IDeveloper>
+    {
+        private string tool;
+        private string name;
+
+        public string Tool { 
+            get { return tool; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Builder(string name, string tool)
+        {
+            this.tool = tool;
+            this.name = name;
+
+        }
+        public void Create()
+        {
+            Console.WriteLine($"The builder {name} uses a {tool} for his work");
+        }
+        public void Destroy()
+        {
+            Console.WriteLine($"The builder {name} has been removed from our database");
+            tool = "";
+            name = "";
+        }
+        public IDeveloper Print(int i)
+        {
+            Console.WriteLine($"Enter developer {i} name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Enter the tool that {name} uses : ");
+            string tool = Console.ReadLine();
+
+            IDeveloper dev = new Builder(name, tool);
+            return dev;
+        }
+        int IComparable<IDeveloper>.CompareTo(IDeveloper other)
+        {
+            return String.Compare(this.Tool, other.Tool);
+        }
+    }
+}
---
+++ Ren_Shev-HW/Homework5/Homework5/Programmer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5
+{
+    internal class Programmer: IDeveloper, IComparable<IDeveloper>
+    {
+        private string name;
+        private string language;
+
+        public string Tool
+        {
+            get { return language; }
+            set { language = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Programmer(string name, string language)
+        {
+            this.language = language;
+            this.name = name;
+
+        }
+        public void Create()
+        {
+            Console.WriteLine($"The programmer {name} has excellent knowledge of {language}");
+        }
+        public void Destroy()
+        {
+            Console.WriteLine($"The programmer {name} has been removed from our database");
+            language = "";
+            name = "";
+        }
+        public IDeveloper Print(int i)
+        {
+            Console.WriteLine($"Enter developer {i} name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Enter a programming language that {name} knows : ");
+            string language = Console.ReadLine();
+
+            IDeveloper dev = new Programmer(name, language);
+            return dev;
+        }
+        int IComparable<IDeveloper>.CompareTo(IDeveloper other)
+        {
+            return String.Compare(this.Tool, other.Tool);
+        }
+    }
+}
