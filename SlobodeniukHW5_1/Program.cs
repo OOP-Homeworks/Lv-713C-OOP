7e6a14780524b2ee974836f475ce614e765ed9a3
Yevheniia Slobodeniuk <90610256+slobodeniukY@users.noreply.github.com>
2022-09-19 13:49:07 +0300

    added homework 5
    added homework 5

---
+++ SlobodeniukHW5_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlobodeniukHW5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IDeveloper> developers = new List<IDeveloper>();
+            developers.Add(new Programmer("Java"));
+            developers.Add(new Programmer("C++"));
+            developers.Add(new Programmer("C#"));
+            developers.Add(new Programmer("Python"));
+            developers.Add(new Builder("Visual Stuio"));
+            developers.Add(new Builder("PyCharm"));
+            developers.Add(new Builder("Eclipse"));
+
+            foreach (IDeveloper develop in developers)
+            {
+                develop.Create();
+                develop.Destroy();
+            }
+
+
+            developers.Sort();
+            foreach (IDeveloper develop in developers)
+            {
+                develop.Create();
+                develop.Destroy();
+            }
+
+        }
+    }
+}
---
+++ SlobodeniukHW5_1/Programmers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace SlobodeniukHW5
+{
+    internal class Programmer : IDeveloper, IComparable<IDeveloper>
+    {
+        private string language;
+        public string Tool
+        {
+            get { return language; }
+            set { language = value; }
+        }
+
+        public Programmer(string language)
+        {
+            this.language = language;
+        }
+
+
+
+        public void Create()
+        {
+            Console.WriteLine($"Create Programmer with programming language: '{Tool}'");
+        }
+
+        public void Destroy()
+        {
+            Console.WriteLine($"Destroy Programmer with programming language: '{Tool}'");
+        }
+
+        int IComparable<IDeveloper>.CompareTo(IDeveloper other)
+        {
+            return String.Compare(this.Tool, other.Tool);
+        }
+    }
+}
