1c47336960cf2af2a963f3bacb47349af2596d2c
Marian Chornobai <112644522+itblackbai@users.noreply.github.com>
2022-09-20 18:13:38 +0300

    Home work 5 + 1

---
+++ hwchornobai/hmw1_task1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace hmw1_tasks1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // task 1
+
+
+            Console.WriteLine("Ведіть число а");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ведіть число b");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("A+B");
+            Console.WriteLine($" A+B =  {a}+{b}={a + b}");
+            Console.WriteLine($" A-B =  {a}-{b}={a - b}");
+            Console.WriteLine($" A*B =  {a}*{b}={a * b}");
+            Console.WriteLine($" A/B =  {a}/{b}={a / b}");
+            Console.ReadLine();
+
+
+
+
+            // task 2
+            Console.WriteLine("How are you?");
+            string answer = Console.ReadLine();
+            Console.WriteLine($"You are {answer}");
+            Console.ReadLine();
+
+
+            //Task 3
+            Console.WriteLine("Ведіть символ 1 ");
+            char symbol1 = char.Parse(Console.ReadLine());
+            Console.WriteLine("Ведіть символ 2 ");
+            char symbol2 = char.Parse(Console.ReadLine());
+            Console.WriteLine("Ведіть символ 3 ");
+            char symbol3 = char.Parse(Console.ReadLine());
+            Console.WriteLine($"You enter" + "   first char == " + symbol1 + "   second char == " + symbol2 + " 3 char ==" + symbol3);
+
+            Console.ReadKey();
+
+            //Task 4
+            Console.WriteLine("Enter integer number =  ");
+            double integernum1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter integer number =  ");
+            double integernum2 = double.Parse(Console.ReadLine());
+            if ((integernum1 > 0) && (integernum2 > 0))
+            {
+                Console.WriteLine("THIS INTEGER IS TRUE");
+            }
+            else
+            {
+                Console.WriteLine("THIS INTEGER ISN'T TRUE");
+            }
+            Console.ReadLine();
+
+            //Homework 1
+            Console.WriteLine("Enter length a = ");
+            double a = double.Parse(Console.ReadLine());
+            double area = a * a;
+            double perimeter = 4 * a;
+            Console.WriteLine($"Area is = " + area);
+            Console.WriteLine($"Perimeter is = " + perimeter);
+            Console.ReadLine();
+
+            //Homework 2
+            Console.WriteLine("Hello, what is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("How old are you, " + name);
+            string age = Console.ReadLine();
+            Console.WriteLine("Name is = " + name + "     Age is =      " + age);
+            Console.ReadKey();
+
+
+            //Homework 3
+            Console.WriteLine("Enter radius of a circle ");
+            double radius = double.Parse(Console.ReadLine());
+            double pi = 3.14;
+            double length = (2 * pi * radius);
+            double area = (pi * radius * radius);
+            double volume = (4 / 3 * pi * radius * radius * radius);
+            Console.WriteLine("Length is = " + length);
+            Console.WriteLine("Area is = " + area);
+            Console.WriteLine("Volume is = " + volume);
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ hwchornobai/hw5.1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Numerics;
+
+namespace hm5
+{
+    interface IDeveloper
+    {
+        void Create();
+        void Destroy();
+
+    }
+    class Programer : IDeveloper
+    {
+        private string name;
+        private string language;
+        public Programer(string name, string language)
+        {
+            this.name = name;
+            this.language = language;
+        }
+        public void Create()
+        {
+            Console.WriteLine("The developer : {0} create on {1}", name, language);
+        }
+        public void Destroy()
+        {
+            Console.WriteLine("The developer : {0} destroy on {1}", name, language);
+        }
+     class Builder : IDeveloper
+        {
+            private string name;
+            private string tool;
+            public Builder(string name, string tool)
+            {
+                this.name = name;
+                this.tool = tool;
+            }
+            public void Create()
+            {
+                Console.WriteLine("The developer : {0} create with the {1}", name, tool);
+            }
+            public void Destroy()
+            {
+                Console.WriteLine("The developer : {0} destroy with the {1}", name, tool);
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                List<IDeveloper> worklist = new List<IDeveloper>();
+                worklist.Add(new Programer("ANDRII", "С#"));
+                worklist.Add(new Programer("VASIL", "С++"));
+                worklist.Add(new Builder("Roma", "Plutochnik"));
+                worklist.Add(new Builder("OLEG", "KABELICHUK"));
+                for (int i = 0; i < worklist.Count; i++)
+                { 
+                        worklist[i].Create();
+                }
+               
+                for (int i = 0; i <worklist.Count; i++)
+                {
+                    worklist[i].Destroy();
+                }
+                foreach(IDeveloper b in worklist)
+                {
+                    Console.WriteLine(b);
+                }
+                                
+
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
