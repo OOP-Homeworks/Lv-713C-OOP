03dafadf2dc795d92ce3b5129ac8cfc01ffb3a4c
Maria Scheglova <91660359+Marichka2003@users.noreply.github.com>
2022-09-20 22:05:46 +0300

    Added Homework_6

---
+++ Marichka2003_Task_6/Marichka2003_Task_6/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Marichka2003_Task_6
+{
+
+    class Program
+    {
+        public static void Div(int firstNumber, int secondNumber)
+        {
+            again:
+            try
+            {
+                Console.Write($"Dividing : { firstNumber / secondNumber} ");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.Write("Divide by 0\nEnter second number : ");
+                secondNumber = Convert.ToInt32(Console.ReadLine());
+                goto again;
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+        start:
+
+            int firstNumber;
+            int secondNumber;
+            try
+            {
+                Console.Write("Enter the first number : ");
+                firstNumber = Convert.ToInt32(Console.ReadLine());
+            }
+
+            catch (FormatException exception)
+            {
+                Console.WriteLine(exception.Message);
+                goto start;
+            }
+        start2:
+
+            try
+            {
+                Console.Write("Enter the second number : ");
+                secondNumber = Convert.ToInt32(Console.ReadLine());
+            }
+
+            catch (FormatException exception)
+            {
+                Console.WriteLine(exception.Message);
+                goto start2;
+            }
+
+            Div(firstNumber, secondNumber);
+        }
+    }      
+}
