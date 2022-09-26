90851464ea693a8dcd1d837427cbc69dd4c1de15
MaximovichOleg <112766266+MaximovichOleg@users.noreply.github.com>
2022-09-26 12:15:08 +0300

    Adding HW4

---
+++ Maximovich_HW/HW4/HW4/Person.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4
+{
+    internal class Person
+    {
+        private string name;
+        private DateTime birthYear;
+        private int agePerson;
+        public string Name
+        {
+            get { return name; }
+        }
+        public DateTime BirthYear
+        {
+            get { return birthYear; }
+        }
+        public int AgePerson
+        {
+            get { return agePerson; }
+        }
+        public Person(){}
+        public Person(string name, DateTime birthYear, int agePerson)
+        {
+            this.name = name;
+            this.birthYear = birthYear;
+            this.agePerson = agePerson;
+        }
+        public void Input()
+        {
+            Console.Write("enter you name: ");
+            name = Console.ReadLine();
+            Console.Write("enter you year of born: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            birthYear = new DateTime(year, 1, 1);
+        }
+        public void Age()
+        {
+            agePerson = DateTime.Now.Year - birthYear.Year;
+        }
+        public void ChangeName()
+        {
+            if (agePerson < 16)
+            {
+                name = "Very Young";
+            }
+        }
+        public override string ToString()
+        {
+            return $"You name: {name}, your age: {agePerson} years";
+        }
+        public string Output()
+        {
+            return ToString();
+        }
+
+        public static bool operator ==(Person first, Person second)
+        {
+            return (first.name == second.name);
+        }
+
+        public static bool operator !=(Person first, Person second)
+        {
+            return !(first.name == second.name);
+        }
+    }
+}
---
+++ Maximovich_HW/HW4/HW4/Program.cs
@@ -0,0 +1,45 @@
+﻿
+namespace HW4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] person = new Person[3];
+            for (int i = 0; i < person.Length; i++)
+            {
+                person[i] = new Person();
+                person[i].Input();
+                person[i].Age();
+            }
+            for (int i = 0; i < person.Length; i++)
+            {
+                if (person[i].AgePerson < 16)
+                {
+                    person[i].ChangeName();
+                }
+                Console.WriteLine(person[i].Output());
+            }
+            Console.WriteLine("_______________");
+            for (int i = 0; i < person.Length; i++)
+            {
+                for (int j = i; j < person.Length; j++)
+                {
+                    if (person[i].Name == person[j].Name)
+                    {
+                        Console.WriteLine("Person {0} and {1} is a Equals", person[i].Name, person[j].Name);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not equals");
+                    }
+                }
+            }
+            Console.WriteLine("_______________");
+            for (int i = 0; i < person.Length; i++)
+            {
+                Console.WriteLine(person[i].Output());
+            }
+        }
+    }
+}
