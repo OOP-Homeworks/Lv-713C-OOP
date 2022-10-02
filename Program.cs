8d52798d0abfb287d4654a59f12c7c4fc430487d
Yevheniia Slobodeniuk <90610256+slobodeniukY@users.noreply.github.com>
2022-10-02 17:59:38 +0300

    added homework Slobodeniuk_HW8

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlobodeniukHW8
+{
+    internal class Program
+    {
+        private static void InputShapes(List<Shape> shape)
+        {
+            int count = 10;
+            int id;
+            double lenth;
+            for (int i = 0; i < count; i++)
+            {
+                a1:
+                try
+                {
+                    Console.WriteLine("Enret shape: '1' for circule or '2' for square");
+                    id = Convert.ToInt32(Console.ReadLine());
+
+                    switch (id)
+                    {
+                         case 1:
+                            {
+                                Console.WriteLine("Enter the radius of the circle: ");
+                                lenth = Convert.ToInt32(Console.ReadLine());
+                                shape.Add(new Circle(lenth, nameof(Circle)));
+                                break;
+                            }
+
+                         case 2:
+                            {
+                                Console.WriteLine("Enter the side of the square: ");
+                                lenth = Convert.ToInt32(Console.ReadLine());
+                                shape.Add(new Square(lenth, nameof(Square)));
+                                break;
+                            }
+                        default:
+                            {
+                                Console.WriteLine("You need to choose some shape!");
+                                goto a1;
+                            }
+                    }
+                 
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Wrong format");
+                    goto a1;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Shape> shapes = new List<Shape>();
+            InputShapes(shapes);
+
+            foreach (var shape in shapes)
+            {
+                shape.GetArea();
+                shape.GetPerimeter();
+                shape.Print();
+
+             
+            }
+
+            double max = 0;
+            string name = String.Empty;
+
+            foreach (var shape in shapes)
+            {
+                if (shape.Perimeter > max)
+                {
+                    max = shape.Perimeter;
+                    name = shape.Name;
+                }
+            }
+            Console.WriteLine("************************************");
+            Console.WriteLine("Name is: {0} with max perimetr: {1}", name, max);
+
+
+            Console.WriteLine("************************************");
+            Console.WriteLine("Sort shapes");
+            shapes.Sort();
+
+            foreach (var shape in shapes)
+            {
+                shape.Print();
+            }
+
+        }
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlobodeniukHW8
+{
+    public class Square : Shape
+    {
+        private double side;
+
+        public override double Perimeter { get; set; }
+        public override double Area { get; set; }
+
+        public Square(double side, string name) : base(name)
+        {
+            this.side = side;
+        }
+
+        public override void GetPerimeter()
+        {
+            Perimeter = side * 4;
+        }
+
+        public override void GetArea()
+        {
+            Area = Math.Pow(side, 2);
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Name is: " + Name + "; Area is: " + Area + "; Perimeter is: " + Perimeter);
+        }
+
+        
+
+    }
+}
