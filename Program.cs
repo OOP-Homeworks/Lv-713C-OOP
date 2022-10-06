216a74868cf148e5bd39167ea254444666073081
Maria Scheglova <91660359+Marichka2003@users.noreply.github.com>
2022-10-06 16:08:52 +0300

    Marichka2003 Added HW10

--- Program.cs
+++ Program.cs
@@ -1,97 +1,110 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace SlobodeniukHW8
+namespace Marichka2003_Homework_10
 {
-    internal class Program
+    public class Person
     {
-        private static void InputShapes(List<Shape> shape)
+        private string name;
+        private DateTime birthYear;
+        public int age;
+
+        public Person() { }
+
+        public Person(string name)
         {
-            int count = 10;
-            int id;
-            double lenth;
-            for (int i = 0; i < count; i++)
+            this.name = name;
+        }
+        public struct People
+        {
+            public string name;
+            public override string ToString()
             {
-                a1:
-                try
-                {
-                    Console.WriteLine("Enret shape: '1' for circule or '2' for square");
-                    id = Convert.ToInt32(Console.ReadLine());
+                return $"({name})";
+            }
+            public People(string name)
+            {
+                this.name = name;
+            }
+        }
+     
+        public int Age(DateTime birthYear)
+        {
+            age = DateTime.Now.Year - birthYear.Year;
 
-                    switch (id)
-                    {
-                         case 1:
-                            {
-                                Console.WriteLine("Enter the radius of the circle: ");
-                                lenth = Convert.ToInt32(Console.ReadLine());
-                                shape.Add(new Circle(lenth, nameof(Circle)));
-                                break;
-                            }
+            if (DateTime.Now.Month < birthYear.Month)
+            {
+                age -= 1;
+            }
+            return age;
+        }
 
-                         case 2:
-                            {
-                                Console.WriteLine("Enter the side of the square: ");
-                                lenth = Convert.ToInt32(Console.ReadLine());
-                                shape.Add(new Square(lenth, nameof(Square)));
-                                break;
-                            }
-                        default:
-                            {
-                                Console.WriteLine("You need to choose some shape!");
-                                goto a1;
-                            }
-                    }
-                 
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("Wrong format");
-                    goto a1;
-                }
+        public void Input(int i)
+        {
+            Console.Write($"Enter {i + 1} person's name : ");
+            name = Console.ReadLine();
+            Console.Write($"Enter {i + 1} person's birthday in format dd/mm/yyyy : ");
+            birthYear = Convert.ToDateTime(Console.ReadLine());
+            while (birthYear.Year > DateTime.Now.Year)
+            {
+                Console.WriteLine("You Enter wrong date!");
+                Console.Write($"Enter {i + 1} person`s birthday in format dd/mm/yyyy : ");
+                birthYear = Convert.ToDateTime(Console.ReadLine());
+            }
+        }
+        public string ChangeName(int age)
+        {          
+            if (age < 18)
+            {
+                return "Very Young";
+            }
+            else
+            {
+                return "Adult";
             }
         }
 
+        public override string ToString()
+        {
+            return name;
+        }
+        public string Output()
+        {
+            return name.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Person person &&
+                   name == person.name &&
+                   birthYear == person.birthYear &&
+                   age == person.age;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1346232979;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(name);
+            hashCode = hashCode * -1521134295 + birthYear.GetHashCode();
+            hashCode = hashCode * -1521134295 + age.GetHashCode();
+            return hashCode;
+        }
+
+        public static bool operator ==(Person first, Person second)
+        {
+            return (first.name == second.name);
+        }
+
+        public static bool operator !=(Person first, Person second)
+        {
+            return !(first.name == second.name);
+        }
+    }
+    class Program
+    {
         static void Main(string[] args)
         {
-            List<Shape> shapes = new List<Shape>();
-            InputShapes(shapes);
-
-            foreach (var shape in shapes)
-            {
-                shape.GetArea();
-                shape.GetPerimeter();
-                shape.Print();
-
-             
-            }
-
-            double max = 0;
-            string name = String.Empty;
-
-            foreach (var shape in shapes)
-            {
-                if (shape.Perimeter > max)
-                {
-                    max = shape.Perimeter;
-                    name = shape.Name;
-                }
-            }
-            Console.WriteLine("************************************");
-            Console.WriteLine("Name is: {0} with max perimetr: {1}", name, max);
-
-
-            Console.WriteLine("************************************");
-            Console.WriteLine("Sort shapes");
-            shapes.Sort();
-
-            foreach (var shape in shapes)
-            {
-                shape.Print();
-            }
-
-        }
+                       
+        }    
     }
 }
