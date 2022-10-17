a91bfa151e955cc883e392a48201d9f5140df8d1
OlegShokun <shokunoleg@gmail.com>
2022-10-17 19:49:36 +0300

    add HW10

---
+++ ShokunHomework/HW10/ShokunHW10_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShokunHW10_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1, p2, p3, p4, p5, p6, p7, p8, p9;
+            p1 = new Point(10, 20);
+            p2 = new Point(-2, 53);
+            p3 = new Point(3, 4);
+            p4 = new Point(4, 5);
+            p5 = new Point(5, 6);
+            p6 = new Point(6, 7);
+            p7 = new Point(1, 5);
+            p8 = new Point(2, 7);
+            p9 = new Point(3, 1);
+
+            List<Triangle> triangles = new List<Triangle>();
+
+            triangles.Add(new Triangle(p1, p2, p3));
+            triangles.Add(new Triangle(p4, p5, p6));
+            triangles.Add(new Triangle(p7, p8, p9));
+
+            foreach(Triangle triangle in triangles)
+            {
+                triangle.Print();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ShokunHomework/HW10/ShokunHW10_1/Triangle.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShokunHW10_1
+{
+    public struct Point
+    {
+        public int X, Y;
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+
+    public class Triangle
+    {
+
+        public Point vertex1, vertex2, vertex3;
+
+        public Triangle(Point vertex1, Point vertex2, Point vertex3)
+        {
+            this.vertex1 = vertex1;
+            this.vertex2 = vertex2;
+            this.vertex3 = vertex3;
+        }
+
+        public double Distance(Point p1, Point p2)
+        {
+            return Math.Round(Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2)), 2);
+        }
+
+        public double Perimeter()
+        {
+            return Math.Round(Distance(vertex1, vertex2) + Distance(vertex2, vertex3) + Distance(vertex1, vertex3), 2);
+        }
+
+        public double Square()
+        {
+            double ab = Distance(vertex1, vertex2);
+            double bc = Distance(vertex2, vertex3);
+            double ac = Distance(vertex1, vertex3);
+            double p = Perimeter() / 2;
+            return Math.Round(Math.Sqrt(p * (p - ab) * (p - bc) * (p - ac)), 2);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"\nVertex1: {vertex1.ToString()}\nVetrex2: {vertex2.ToString()}\nVertex3: {vertex3.ToString()}\nPerimeter: {Perimeter()}\nSquare: {Square()}");
+        }
+    }
+}
